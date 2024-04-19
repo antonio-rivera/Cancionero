a7d8df08bccbecd34c1a0ba0d7c42b5b598a26a0
Antonio <antoniorivera.cs18@gmail.com>
2024-04-18 20:08:37 -0400

    Added react and sqlite3 and tested they are at least not producing any errors. This is the inital commit.

--- vite.preload.config.ts
+++
@@ -1,8 +0,0 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-
-// https://vitejs.dev/config
-export default defineConfig({
-  base: './',
-  root: path.join(__dirname, 'src', 'preload'),
-})
