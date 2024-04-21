d9c71662825a4d78bd42c8195a799ab5e1be6fd5
Antonio <antoniorivera.cs18@gmail.com>
2024-04-21 08:47:38 -0400

    Tested succesfully the retrieval of data.

--- my-new-app/src/App.tsx
+++ my-new-app/src/App.tsx
@@ -1,5 +1,17 @@
- async function App() {
-   const data = await window.DB.getAllData()
+import { useEffect, useState } from "react"
+import { Song} from "./models/Song";
+
+ function App() {
+  const [data, setData] = useState<Array<Song>>(null); 
+
+   useEffect( () => {
+    const fetchData = async () => {
+      const response: Array<Song> = await window.DB.getAllData()
+      setData(response)
+    }
+   fetchData() 
+   }, [])
+
   return (
     <>
     <h1>Hello from React</h1>
