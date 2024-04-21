e7df79f218d43ffaddccd20638006dbc255e5147
Antonio <antoniorivera.cs18@gmail.com>
2024-04-21 10:07:27 -0400

    added nonfunctional basic header component

---
+++ my-new-app/src/components/Header.tsx
@@ -0,0 +1,12 @@
+export function Header()
+{
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light btn-primary">
+    <div className="navbar-nav">
+      <a className="mx-2 h4 nav-item nav-link text-white" href="search.html">Buscar</a>
+      <a className="mx-2 h4 nav-item nav-link text-white" href="#">Explorar</a>
+      <a className="mx-2 h4 nav-item nav-link text-white" href="#">Añadir</a>
+    </div>
+    </nav>
+  )
+}
