4b89f3b7bcd7eefd7b23e4f5279f2e4d3bad970f
Antonio <antoniorivera.cs18@gmail.com>
2024-04-21 07:27:05 -0400

    Unnecessary import of sqlite3 causing error of native module not built.

--- my-new-app/src/preload.ts
+++ my-new-app/src/preload.ts
@@ -1,14 +1,4 @@
 import { contextBridge, ipcRenderer } from "electron";
-import sqlite3 from "sqlite3";
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-
-const db = new sqlite3.Database('./songs.db', err => {
-  if (err) {
-      return console.error('Database connection failed!', err);
-  }
-  console.log('Connected to SQLite database.');
-});
 
 contextBridge.exposeInMainWorld("DB", {
  getAllData: () => ipcRenderer.invoke("getall")
