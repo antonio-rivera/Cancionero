7086ebd127f00e06304e0704f63e55cc14beefb0
Antonio <antoniorivera.cs18@gmail.com>
2024-04-21 20:06:43 -0400

    Adding functionality for searching inside search component.

--- my-new-app/src/components/SearchForm.tsx
+++ my-new-app/src/components/SearchForm.tsx
@@ -1,11 +1,29 @@
-export function SearchForm()
-{
+import { useState } from "react";
+import { Song } from "../models/Song";
+import { searchUtilsModule } from "../utils/searchUtils";
+
+type Props =
+  {
+    songs: Array<Song>
+  }
+
+export function SearchForm({ songs }: Props) {
+  const [query, setQuery] = useState<string>("")
+  let searchResults;
+  function handleSubmit(event: React.MouseEvent) {
+    event.preventDefault();
+    if (query) {
+      searchResults = searchUtilsModule.matchResults(query, songs)
+    }
+
+  }
+
   return (
     <form className="my-5 text-center" id="search-form">
       <h3 className="pb-2">Busqueda</h3>
-      <input placeholder="Busca aqui la informacion de la cancion..." className="form-control" type="search" name="search"
-        id="search" />
-      <button className="btn btn-primary mt-3" id="search-button" type="submit">Buscar</button>
+      <input placeholder="Busca aqui la informacion de la cancion..." className="form-control w-75 mx-auto" type="search" name="search"
+        id="search" value={query} onChange={(event => setQuery(event.target.value))} />
+      <button className="btn btn-primary mt-3" id="search-button" type="submit" onClick={(event) => handleSubmit(event)}>Buscar</button>
     </form>
   )
 }
