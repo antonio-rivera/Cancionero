0ef2b50bc14027867aefc105cbf97218b24a2b0e
Antonio <antoniorivera.cs18@gmail.com>
2024-04-27 09:33:23 -0400

    Rendered results for search query, functionality is working as expected

--- my-new-app/src/components/TableResults.tsx
+++ my-new-app/src/components/TableResults.tsx
@@ -7,18 +7,22 @@ type Props =
   }
 
 export function TableResults({ haveResults, results }: Props) {
-  // TODO: Render results array table as a map 
-  //       function with the data. 
-  // TODO: Resize table
   if (haveResults) {
     return (
-      <table className="table">
+      <table className="table w-75 mx-auto">
         <thead>
           <tr>
             <th scope="col">Titulo</th>
             <th scope="col">Artista</th>
             <th scope="col">Genero</th>
           </tr>
+          {results.map(song =>
+            <tr key={song.ID}>
+              <td>{song.title}</td>
+              <td>{song.artist}</td>
+              <td>{song.genre}</td>
+            </tr>
+          )}
         </thead>
         <tbody>
 
