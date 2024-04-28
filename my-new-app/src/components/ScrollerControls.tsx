e31bdb2da339e5be7df9bf1256e584cafdad41ba
Antonio <antoniorivera.cs18@gmail.com>
2024-04-28 17:27:53 -0400

    Added a song footer that controls auto scrolling

---
+++ my-new-app/src/components/ScrollerControls.tsx
@@ -0,0 +1,10 @@
+export function ScrollerControls() {
+  return <>
+    <div id="song-footer" className="bg-primary d-flex justify-content-center align-items-center">
+      <div className="row m-auto">
+        <div className="h3 text-white text-center mr-3">Empezar desplazamiento: </div>
+        <button className="btn btn-success rounded">Velocidad 1.0</button>
+      </div>
+    </div>
+  </>
+}
