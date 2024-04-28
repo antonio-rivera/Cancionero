1ae9e193524f917bdd58c2c4faa730299be280de
Antonio <antoniorivera.cs18@gmail.com>
2024-04-28 10:08:49 -0400

    Added routes for existing components

--- my-new-app/src/App.tsx
+++ my-new-app/src/App.tsx
@@ -3,6 +3,8 @@ import { Song } from "./models/Song";
 import { Header } from "./components/Header"
 import "/Users/antonio/Desktop/WebDevStuff/React/cancionero/my-new-app/bootstrap/css/bootstrap.min.css"
 import { SearchForm } from "./components/SearchForm";
+import { Route, Routes } from "react-router-dom";
+import { SongView } from "./components/SongView";
 function App() {
   const [data, setData] = useState<Array<Song>>(null);
 
@@ -17,7 +19,10 @@ function App() {
   return (
     <>
       <Header />
-      <SearchForm songs={data} />
+      <Routes>
+        <Route path="/" element={<SearchForm songs={data} />} />
+        <Route path="/song/:id" element={<SongView songs={data} />} />
+      </Routes>
     </>
   )
 }
