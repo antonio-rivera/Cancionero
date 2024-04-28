b26de9f5408e715b0a387f8b67c0e2afc9fc5848
Antonio <antoniorivera.cs18@gmail.com>
2024-04-28 10:29:57 -0400

    Added song values to page. Need to style it now.

--- my-new-app/src/components/SongView.tsx
+++ my-new-app/src/components/SongView.tsx
@@ -2,9 +2,25 @@ import { Props } from "../../interface"
 import { useParams } from "react-router-dom"
 export function SongView({ songs }: Props) {
   const { id } = useParams()
-  console.log(id);
+  if (songs) {
+    const currentSong = songs.find(song => song.ID === Number(id))
+    if (currentSong) {
+      const { title, artist, lyrics } = currentSong;
+      return <>
+        <div className="card">
+          <div className="card-body">
+            <h1 className="text-center card-title">{title}</h1>
+            <h3 className="card-subtitle text-center">{artist}</h3>
+            <div className="container">
+              <p className="pt-5 card-text text-center">
+                {lyrics}
+              </p>
+            </div>
+          </div>
+        </div>
+      </>
 
-  return <>
-    <h1>Song view</h1>
-  </>
+    }
+
+  }
 }
