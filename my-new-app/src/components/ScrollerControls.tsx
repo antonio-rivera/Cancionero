46943a1ae220d001e5a0a49f681f226c2ee542c2
Antonio <antoniorivera.cs18@gmail.com>
2024-04-29 21:36:57 -0400

    Added better smoothness and dynamics to scrolling

--- my-new-app/src/components/ScrollerControls.tsx
+++ my-new-app/src/components/ScrollerControls.tsx
@@ -2,12 +2,18 @@ export function ScrollerControls() {
   let clearId: string | number | NodeJS.Timeout;
   let startedScrolling: boolean = false;
 
-  function startAutoScroll(velocity: number) {
+  function startAutoScroll(interval: number) {
+    let changeSpeed = true;
+    let velocity = 1;
     stopScrolling();
     startedScrolling = true;
     let scrollAmount = 0;
     window.scrollTo(0, 0);
     clearId = setInterval(() => {
+      if (changeSpeed && window.scrollY >= Math.floor(window.innerHeight / 2)) {
+        interval = interval - 50;
+        changeSpeed = false;
+      }
       let prevScrollY = window.scrollY;
       scrollAmount = scrollAmount + velocity;
       window.scrollTo(0, scrollAmount)
@@ -16,21 +22,22 @@ export function ScrollerControls() {
         clearInterval(clearId);
         startedScrolling = false;
       }
-    }, 500)
+    }, interval)
   }
 
   function stopScrolling() {
     if (startedScrolling && clearId !== undefined) {
       clearInterval(clearId);
+      startedScrolling = false;
     }
   }
   return <>
     <div id="song-footer" className="bg-white d-flex justify-content- align-items-center">
       <div className="row m-auto">
         <div className="h4 text-dark text-center mr-3">Empezar desplazamiento: </div>
-        <button onClick={() => startAutoScroll(3)} className="btn btn-outline-dark rounded">Velocidad 1.0</button>
-        <button onClick={() => startAutoScroll(4)} className="btn btn-outline-dark rounded">Velocidad 2.0</button>
-        <button onClick={() => startAutoScroll(4.5)} className="btn btn-outline-dark rounded">Velocidad 3.0</button>
+        <button onClick={() => startAutoScroll(150)} className="btn btn-outline-dark rounded">Velocidad 1.0</button>
+        <button onClick={() => startAutoScroll(100)} className="btn btn-outline-dark rounded">Velocidad 2.0</button>
+        <button onClick={() => startAutoScroll(50)} className="btn btn-outline-dark rounded">Velocidad 3.0</button>
         <button onClick={stopScrolling} className="btn btn-danger rounded">Detener desplazamiento</button>
       </div>
 
