eada4f587f9d6d9405ba12a6333f35a203901972
Antonio <antoniorivera.cs18@gmail.com>
2024-04-30 17:14:00 -0400

    Removed btn styling

--- my-new-app/src/components/Header.tsx
+++ my-new-app/src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export function Header() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light btn-primary">
+    <nav className="navbar navbar-expand-lg navbar-light bg -primary">
       <div className="navbar-nav">
         <Link to={"/"} className="mx-2 h4 nav-item nav-link text-white">Buscar</Link>
         <a className="mx-2 h4 nav-item nav-link text-white" href="#">Explorar</a>
