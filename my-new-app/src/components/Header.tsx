95bb3c2cd09bca0a71686f2674e061a349105beb
Antonio <antoniorivera.cs18@gmail.com>
2024-04-30 20:36:00 -0400

    Added link in header to new browse page/component.

--- my-new-app/src/components/Header.tsx
+++ my-new-app/src/components/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
     <nav className="navbar navbar-expand-lg navbar-light bg-primary">
       <div className="navbar-nav">
         <Link to={"/"} className="mx-2 h4 nav-item nav-link text-white">Buscar</Link>
-        <a className="mx-2 h4 nav-item nav-link text-white" href="#">Explorar</a>
+        <Link to={"/browse"} className="mx-2 h4 nav-item nav-link text-white">Explorar</Link>
         <a className="mx-2 h4 nav-item nav-link text-white" href="#">Añadir</a>
       </div>
     </nav>
