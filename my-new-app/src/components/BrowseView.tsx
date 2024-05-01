5e7708637701fcb1738f93d1dee413fd9706a1fb
Antonio <antoniorivera.cs18@gmail.com>
2024-04-30 20:34:56 -0400

    Added component to browse songs in a grid style view page.

---
+++ my-new-app/src/components/BrowseView.tsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import { Props } from "../../interface";
+
+export function BrowseView({ songs }: Props) {
+  const navigate = useNavigate();
+  function goToSong(ID: number): void {
+    navigate(`/song/${ID}`)
+  }
+
+  return <>
+    <div className="container-fluid d-flex flex-row flex-wrap justify-content-around align-content-center">
+      {songs.map(song =>
+        <div key={song.ID} onClick={() => goToSong(song.ID)} id="song-card" className="d-flex justify-content-center flex-column rounded p-2 m-2">
+          <div id="song-card-title" className="text-center py-2">{song.title}</div>
+          <div className="text-center text-muted">{song.artist}</div>
+          <div className="text-center text-muted pt-2">{song.genre}</div>
+        </div>
+      )}
+    </div>
+  </>
+
+}
