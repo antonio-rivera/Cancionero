2003f3054ded05cbff1f03cccbe88ee9b4a956fc
Antonio <antoniorivera.cs18@gmail.com>
2024-05-07 12:44:23 -0400

    Removed unneccesary stuff from add song

--- my-new-app/src/components/AddSong.tsx
+++ my-new-app/src/components/AddSong.tsx
@@ -1,23 +1,9 @@
 import { ChangeEvent, FormEvent, useState } from 'react';
 import { Song } from "../models/Song";
 import { UpdateProps } from "../../interface";
-import { useParams } from "react-router-dom";
 import { SongForm } from "./SongForm";
 
 export function AddSong({ updateData }: UpdateProps) {
-
-  // const { id } = useParams();
-  // const objectTemplate: Song = {
-  //   ID: null,
-  //   title: '',
-  //   artist: '',
-  //   genre: '',
-  //   lyrics: ''
-  // }
-
-  //TODO: Add a condition to check the ID, 
-  // search for the song with the corresponding ID
-  // If that song is not found, use the empty object template
 
   const [formData, setFormData] = useState<Song>({
     ID: null,
