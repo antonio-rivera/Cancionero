a6b8b9fb16c02b32c80f9e64d37a552e005838f4
Antonio <antoniorivera.cs18@gmail.com>
2024-05-09 15:19:27 -0400

    Fixed midscroll speed change

--- my-new-app/src/components/ScrollerControls.tsx
+++ my-new-app/src/components/ScrollerControls.tsx
@@ -6,16 +6,31 @@ export function ScrollerControls({ id }: IDProps) {
   let clearId: string | number | NodeJS.Timeout;
   let startedScrolling: boolean = false;
 
-  function startAutoScroll(interval: number) {
-    let changeSpeed = true;
+  function startAutoScroll(interval: number, changeSpeed = true) {
     let velocity = 1;
     stopScrolling();
     startedScrolling = true;
     let scrollAmount = window.scrollY;
+    const rootHeight = document.getElementById("root").scrollHeight;
+    const avgHeight = Math.floor(rootHeight + window.innerHeight) / 2
     clearId = setInterval(() => {
-      if (changeSpeed && window.scrollY >= Math.floor(window.innerHeight / 2)) {
-        interval = interval - 50;
-        changeSpeed = false;
+      if (changeSpeed && window.scrollY >= Math.ceil(avgHeight / 2)) {
+        console.log("Changed speed");
+
+        switch (interval) {
+          case 150:
+            startAutoScroll(interval - 40, false);
+            break;
+          case 100:
+            startAutoScroll(interval - 30, false);
+            break;
+          case 50:
+            startAutoScroll(interval - 25, false);
+            break;
+          default:
+            break;
+        }
+        return;
       }
       let prevScrollY = window.scrollY;
       scrollAmount = scrollAmount + velocity;
@@ -35,13 +50,15 @@ export function ScrollerControls({ id }: IDProps) {
     }
   }
   return <>
-    <div id="song-footer" className="d-flex justify-content-center align-items-center">
+    <div id="song-footer" className="d-flex justify-content-between align-items-center">
       <Link to={`/edit/${id}`}><button id="edit-btn" className="mx-1 btn btn-success">Editar Canción</button> </Link>
-      <div className="h4 text-dark text-center mr-3">Empezar desplazamiento: </div>
-      <button onClick={() => startAutoScroll(150)} className=" btn btn-outline-dark rounded">Velocidad 1.0</button>
-      <button onClick={() => startAutoScroll(100)} className=" btn btn-outline-dark rounded">Velocidad 2.0</button>
-      <button onClick={() => startAutoScroll(50)} className=" btn btn-outline-dark rounded">Velocidad 3.0</button>
-      <button onClick={stopScrolling} className=" btn btn-danger rounded">Detener desplazamiento</button>
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="h5 text-dark text-center mr-3">Desplazamiento: </div>
+        <button onClick={() => startAutoScroll(150)} className=" btn btn-outline-dark rounded">Velocidad 1.0</button>
+        <button onClick={() => startAutoScroll(100)} className=" btn btn-outline-dark rounded">Velocidad 2.0</button>
+        <button onClick={() => startAutoScroll(50)} className=" btn btn-outline-dark rounded">Velocidad 3.0</button>
+        <button onClick={stopScrolling} className=" btn btn-danger rounded mr-1">Detener</button>
+      </div>
     </div>
   </>
 }
