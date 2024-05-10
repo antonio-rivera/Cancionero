e4a40532a3bc0dc832a739e533c2c11395a00f66
Antonio <antoniorivera.cs18@gmail.com>
2024-05-10 17:17:46 -0400

    Fixed searching not capturing second set of vowels.

--- my-new-app/src/utils/searchUtils.ts
+++ my-new-app/src/utils/searchUtils.ts
@@ -5,30 +5,27 @@ interface ISearchUtils {
   matchResults(text: string, songs: Array<Song>): Array<Song>;
 }
 export const searchUtilsModule: ISearchUtils = {
-  matchAccents(searchText: string)
-  {
-      const aclass = /[aáAÁ]/
-      const eclass = /[eéEÉ]/
-      const iclass = /[iíIÍ]/
-      const oclass = /[oóOÓ]/
-      const uclass = /[uúUÚ]/
-      searchText = searchText.replace(aclass, String.raw`${aclass.toString().replaceAll("/", "")}`)
-      searchText = searchText.replace(eclass, String.raw`${eclass.toString().replaceAll("/", "")}`)
-      searchText = searchText.replace(iclass, String.raw`${iclass.toString().replaceAll("/", "")}`)
-      searchText = searchText.replace(oclass, String.raw`${oclass.toString().replaceAll("/", "")}`)
-      searchText = searchText.replace(uclass, String.raw`${uclass.toString().replaceAll("/", "")}`)
-      searchText = searchText.replaceAll(/[nNñÑ]/g, "[nNñÑ]");
-      return searchText;
+  matchAccents(searchText: string) {
+    const aclass = /[aáAÁ]/g
+    const eclass = /[eéEÉ]/g
+    const iclass = /[iíIÍ]/g
+    const oclass = /[oóOÓ]/g
+    const uclass = /[uúUÚ]/g
+    searchText = searchText.replaceAll(aclass, String.raw`${aclass.toString().replaceAll(/[\/g]/g, "")}`)
+    searchText = searchText.replaceAll(eclass, String.raw`${eclass.toString().replaceAll(/[\/g]/g, "")}`)
+    searchText = searchText.replaceAll(iclass, String.raw`${iclass.toString().replaceAll(/[\/g/]/g, "")}`)
+    searchText = searchText.replaceAll(oclass, String.raw`${oclass.toString().replaceAll(/[\/g/]/g, "")}`)
+    searchText = searchText.replaceAll(uclass, String.raw`${uclass.toString().replaceAll(/[\/g/]/g, "")}`)
+    searchText = searchText.replaceAll(/[nNñÑ]/g, "[nNñÑ]");
+    return searchText;
   },
-  matchResults(text, songs) 
-  {
+  matchResults(text, songs) {
     text = this.matchAccents(text);
     return songs.filter(song => {
-      const {title, lyrics, artist} = song;
+      const { title, lyrics, artist } = song;
       const lyricsRe = new RegExp(String.raw`\b${text}\b`, "i")
       const re = new RegExp(`${text}`, "i");
-      if(re.exec(title) || lyricsRe.exec(lyrics) || re.exec(artist))
-      {
+      if (re.exec(title) || lyricsRe.exec(lyrics) || re.exec(artist)) {
         return true;
       }
     })
